a51af86c4ec99919004f75b28ff1f576b376fa47
Alex Romanchenko <monsters.alex0@gmail.com>
2024-12-26 21:36:16 +0300

    feat: map

--- test-houses/app/_components/map/CreateMap.tsx
+++ test-houses/app/_components/map/CreateMap.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker, reactify} from './ymaps';
-import { YMapLocationRequest } from '@yandex/ymaps3-types';
+import './map.scss'
 
-
-const LOCATION: YMapLocationRequest = {
-  center: [37.588144, 55.733842],
-  zoom: 9
-};
-
-export default function CreateMap() {
+export const CreateMap = () => {
   return (
-    <section>
-      тут карта
-      {/* <YMap location={reactify.useDefault(LOCATION)}>
-        <YMapDefaultSchemeLayer />
-        <YMapDefaultFeaturesLayer />
-
-        <YMapMarker coordinates={reactify.useDefault([37.588144, 55.733842])} draggable={true}>
-          <section>
-            <h1>You can drag this header</h1>
-          </section>
-        </YMapMarker>
-      </YMap> */}
+    <section className='map'>
+      <div className='container'>
+        <iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=vulica%20Kirava%208,%20Minsk,%20Minskaja%20voblas%C4%87+(Dinamo%20Stadium)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+      </div>
     </section>
+    
   );
-}
+};
